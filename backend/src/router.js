2a37f26bee3ea0c08539418586d34f05082ba2cc
TomJonesSilva <tom.jones.silva@gmail.com>
2023-11-29 13:47:09 -0300

    correção nas rotas

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,8 +4,8 @@ const usuarioMiddleware = require('./middleweres/usuarioMiddleware');
 const router = express.Router();
 
 router.get('/usuario', usuarioController.getAll);
-router.post('/sigUp', usuarioMiddleware.validateBodyCadastro, usuarioController.sigUp);
-router.post('/sigIn', usuarioMiddleware.validateBodyLogin, usuarioController.sigIn);
+router.post('/login', usuarioMiddleware.validateBodyCadastro, usuarioController.sigUp);
+router.post('/cadastro', usuarioMiddleware.validateBodyLogin, usuarioController.sigIn);
 router.get('/busca',usuarioMiddleware.verifyJTW,usuarioController.buscarUsuario);
 
 module.exports = router;

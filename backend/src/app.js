64449997f07e49ba3549260ccc7835be370dce41
Casalbe <casalbecasos@gmail.com>
2023-12-10 09:58:38 -0300

    Revert do app.js pq eu fui burro e esqueci das convenções do node

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = require('./router');
+const bodyParser = require('body-parser');
 
 
 const app = express();
 
 app.use(express.json());
 app.use(router);
+app.use(bodyParser.json());
+app.use('/stock', stockController);
 
 module.exports = app;
+
